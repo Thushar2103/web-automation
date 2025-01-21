041af79df6c5c2dcd73067c12a7278361b570f7e
Thushar2103 <tthushar2004@gmail.com>
2025-01-21 16:38:58 +0530

    cache clear

--- android/app/src/main/kotlin/com/example/web_automation/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.web_automation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/components/color.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color secondarycolor = Color(0xFFf4f5f9);
